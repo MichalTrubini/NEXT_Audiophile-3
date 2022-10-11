cba27da3c07378d9c9d2eb5f75e328a6fdd51693
Michal Trubini <mchl.trubini@gmail.com>
2022-10-11 22:11:54 +0200

    input WIP

--- src/modules/product/partials/addToCart.tsx
+++ src/modules/product/partials/addToCart.tsx
@@ -3,7 +3,19 @@ import styles from "./addToCart.module.css";
 const AddToCart = () => {
   return (
     <form className={styles.form}>
-      <input type="number" className={styles.input} />
+      <div className={styles.inputContainer}>
+        <p className={`${styles.qtyHandler} ${styles.qtyHandlerMinus}`}>-</p>
+        <input
+          type="number"
+          className={styles.input}
+          step={1}
+          min={0}
+          value={0}
+          readOnly
+        />
+              <p className={`${styles.qtyHandler} ${styles.qtyHandlerPlus}`}>+</p>
+      </div>
+
       <button className="button buttonLight">add to cart</button>
     </form>
   );
